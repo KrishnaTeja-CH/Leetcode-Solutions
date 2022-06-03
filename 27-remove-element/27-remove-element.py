716c4f8923acdd01d04f75f60a9a734fccb3dda3
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2022-06-03 11:20:18 -0500

    Time: 61 ms (18.29%), Space: 13.9 MB (13.46%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -3,7 +3,7 @@ class Solution:
         i = 0
         while(i<len(nums)):
             if nums[i] == val:
-                nums.pop(i)
+                del nums[i]
             else:
                 i += 1
         return len(nums)
