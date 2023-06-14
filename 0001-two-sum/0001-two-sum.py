da2770cba89b751641f63f6b0c2ee6a4ca2b23aa
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-06-13 22:58:13 -0400

    Time: 74 ms (71.33%), Space: 17.7 MB (13.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        solmap = {}
-        for i in range(len(nums)): 
-            diff = target - nums[i] # i =0, nums[i]=2
-            if diff in solmap: # diff = 7
-                return [i, solmap[diff]]
-            solmap[nums[i]] = i # 0:"2" for 1 iteration
+        hashmap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hashmap:
+                return [i, hashmap[diff]]
+            hashmap[nums[i]] = i
