202225cf01e661f326f213d79cff6953c9649ff7
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-02 16:20:48 -0500

    Time: 43 ms (85.33%), Space: 16.3 MB (38.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,11 +4,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         #Selection Sort
-
-        for i in range(len(nums)):
-            minIndex = i
-            for j in range(i+1, len(nums)):
-                if nums[j] < nums[minIndex]:
-                    minIndex = j
-            nums[i], nums[minIndex] = nums[minIndex], nums[i]
+        nums.sort()
         
