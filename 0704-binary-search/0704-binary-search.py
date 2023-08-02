d6f41f3a3f4e5f58034f5e31828e580bbf9957fb
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-02 14:43:16 -0500

    Time: 224 ms (92.76%), Space: 17.8 MB (68.76%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,5 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        #Linear Search
         for i in range(len(nums)):
             if nums[i] == target:
                 return i
