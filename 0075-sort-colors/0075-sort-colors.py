7de488ffd835a6997cb076c82aaa7ec8b46605f1
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-02 16:31:08 -0500

    Time: 52 ms (40.31%), Space: 16.2 MB (72.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,10 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         #Selection Sort
-        for i in range(len(nums)):
-            minIndex = i
-            for j in range(i+1, len(nums)):
-                if nums[j] < nums[minIndex]:
-                    minIndex = j
-            nums[i], nums[minIndex] = nums[minIndex], nums[i]
         
+        
+        #Bubble Sort
+        for i in range(len(nums)):
+            for j in range(1, len(nums)):
+                if nums[j] <= nums[j-1]:
+                    nums[j], nums[j-1] = nums[j-1], nums[j] 
