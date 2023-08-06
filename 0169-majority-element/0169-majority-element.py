24d08a6859e60f0e64e92583628fa6b369aef1a7
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-05 22:59:04 -0500

    Time: 146 ms (96.89%), Space: 17.8 MB (96.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Time - O(n*logn)
-        nums.sort()
-        return nums[len(nums)//2]
+        #nums.sort()
+        #return nums[len(nums)//2]
+
+        # O(n)
+        count = sol = 0
+        for i in nums:
+            if count == 0: sol = i
+            if i == sol: count += 1
+            else: count -= 1
+        return sol
                 
         
         
