6a3f711fe35f3ca79a2952066305b4e08e3ed1d1
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-10 13:34:07 -0500

    Time: 53 ms (54.81%), Space: 16.3 MB (75.80%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.py
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.py
@@ -1,8 +1,8 @@
 class Solution:
     def secondHighest(self, s: str) -> int:
-        num = set()
+        num = []
         for c in s:
-            if c.isnumeric(): num.add(int(c))
-        return sorted(list(num))[-2] if len(list(num)) >= 2 else -1
+            if c.isnumeric(): num.append(int(c))
+        return sorted(list(set(num)))[-2] if len(list(set(num))) >= 2 else -1
 
     
