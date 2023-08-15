57385184cbcd2c58d79caa9268c385f346b9d719
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-15 18:10:53 -0500

    Time: 48 ms (94.95%), Space: 16.6 MB (29.61%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        sol, res = {}, {}
-        for i in ransomNote:
-            sol[i] = sol.get(i, 0) + 1
-        for j in magazine:
-            res[j] = res.get(j, 0) + 1
+        sol = Counter(ransomNote)
+        res = Counter(magazine)
         for k in sol:
             res[k] = res.get(k, 0) - sol[k]
             if res[k] < 0:
