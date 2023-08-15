03dbbcc91d9ff9a8f0f5d38c4a073f50fe47868d
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-15 17:15:02 -0500

    Time: 52 ms (90.82%), Space: 16.5 MB (92.43%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        k = Counter(ransomNote)
-        t = Counter(magazine)
-        for i in k:
-            t[i] = t.get(i, 0) - k[i]
-            if t[i] < 0:
+        s = Counter(ransomNote)
+        m = Counter(magazine)
+        for i in s:
+            m[i] = m.get(i, 0) - s[i]
+            if m[i] < 0:
                 return False
         return True
-        print(t[i])
+        
