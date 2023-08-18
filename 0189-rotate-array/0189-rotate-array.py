b1e29f189457a0971dbfc83096733ff32f60a82e
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-18 00:49:35 -0500

    Time: 181 ms (87.94%), Space: 27.8 MB (36.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,5 +4,4 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k %= len(nums)
-        nums[k:], nums[:k] = nums[:-k], nums[-k:]
-            
+        nums[k:], nums[:k] = nums[:-k], nums[-k:],
