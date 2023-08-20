45d1c0dbc649f2dcec2fa447e7d93b1394426274
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-20 18:51:14 -0500

    Time: 1867 ms (35.94%), Space: 20.8 MB (23.01%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        sol = set()
+        for i in range(len(nums)):
+            first = nums[i]
+            j = i + 1
+            k = len(nums)-1
+            while j < k:
+                second = nums[j]
+                third = nums[k]
+                solution = first + second + third
+                if solution > 0:
+                    k -= 1
+                elif solution < 0:
+                    j += 1
+                else:
+                    sol.add((first, second, third))
+                    j += 1
+                    k -= 1
+        return sol
+
+
+
+                   
