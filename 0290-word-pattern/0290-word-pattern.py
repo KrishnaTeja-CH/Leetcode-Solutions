31b0d63edaea00dc0b2fb50c8849d345970bfabf
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-20 19:17:36 -0500

    Time: 39 ms (72.09%), Space: 16.1 MB (95.40%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,12 +1,12 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        words= s.split(' ')
-        sol, res =[], []
+        sol, res = [], []
+        words = s.split(' ')
         for i in pattern:
             sol.append(pattern.index(i))
         for j in words:
-            res.append(words.index(j))    
-        return res == sol
-       
-       
+            res.append(words.index(j))
+        return sol == res
+        
+
             
