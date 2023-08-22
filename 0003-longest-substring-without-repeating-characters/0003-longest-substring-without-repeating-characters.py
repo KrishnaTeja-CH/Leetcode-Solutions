c6fa5ef4e6813432ec7f4b7d3f92eb7398097858
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-22 18:18:57 -0500

    Time: 59 ms (84.36%), Space: 16.5 MB (11.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,13 +5,8 @@ class Solution:
         for i in range(len(s)):
             while s[i] in sol:
                 sol.remove(s[l])
-                l += 1 
+                l += 1
             sol.add(s[i])
             res = max(res, i-l+1)
         return res
-             
-                
         
-       
-                
-      
