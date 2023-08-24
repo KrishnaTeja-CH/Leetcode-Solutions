0c251143f9fe658317d49b6685a8d346a09ead46
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-23 23:38:24 -0500

    Time: 113 ms (94.08%), Space: 17.2 MB (58.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,18 +5,11 @@ class Solution:
         for i in range(len(numbers)):
             k = target - numbers[i]
             if k in sol:
-                return [sol[k] , i+1]
-            sol[numbers[i]] = i +1
+                return [sol[k]+1, i+1]
+            sol[numbers[i]] = i
+        
+            
 
-# Two Pointer
-        s, t  = 0, len(numbers)-1
-        while s < t:
-            sol = numbers[s] + numbers[t]
-            if sol == target:
-                return [s+1, t+1]
-            elif sol < target :
-                s += 1
-            else:
-                t -= 1
+
                 
                 
