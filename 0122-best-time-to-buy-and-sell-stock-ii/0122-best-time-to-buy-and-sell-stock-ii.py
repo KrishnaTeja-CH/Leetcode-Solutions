8cdbe7ea5fc04267449b22501a25b98271cc4330
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-08-26 12:45:38 -0500

    Time: 52 ms (99.16%), Space: 17.8 MB (60.55%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int: 
-        mini, maxi, sol = sys.maxsize, 0, 0
+        ''''mini, maxi, sol = sys.maxsize, 0, 0
         for i in prices:
             mini = min(i, mini)
             profit = i - mini
@@ -9,6 +9,15 @@ class Solution:
                 sol += profit
             else:
                 maxi = max(profit, maxi)
-        return sol
+        return sol'''
+
+
+#Type - 2 
+        profit = 0
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                profit += prices[i+1] - prices[i]
+        return profit
+ 
             
         
