9f8f33a626ad2017abc012c122c7277a26c3c224
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-09-07 16:46:17 -0500

    Time: 40 ms (67.56%), Space: 16.4 MB (41.10%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -2,8 +2,7 @@ class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1 + str2 != str2 + str1 :
             return ""
-        sol = gcd(len(str1), len(str2))
-        return str1[:sol]
+        return str1[:gcd(len(str1), len(str2))]
         
      
         
