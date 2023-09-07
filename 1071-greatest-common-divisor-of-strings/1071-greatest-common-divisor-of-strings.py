af428aee1b23faae8bcd2ad21e2ae109ff4fb525
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-09-07 16:45:54 -0500

    Time: 33 ms (93.07%), Space: 16.4 MB (41.10%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1 :
+            return ""
+        sol = gcd(len(str1), len(str2))
+        return str1[:sol]
+        
+     
+        
