c77985003fd9353e4d1af79d35fc3f82405e1abc
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-09-11 20:17:09 -0500

    Time: 48 ms (86.69%), Space: 16.6 MB (98.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sol, res = {}, {}
-        for i in s:
-            sol[i] = sol.get(i,0) + 1
-        for j in t:
-            res[j] = res.get(j, 0) + 1
+        sol, res = Counter(s), Counter(t)
+       
         return res == sol
