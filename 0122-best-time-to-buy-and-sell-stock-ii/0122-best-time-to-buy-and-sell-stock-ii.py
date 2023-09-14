3124f0d63c3412bc87153b5c63da0d4bdbc072ea
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-09-13 19:49:54 -0500

    Time: 54 ms (97.33%), Space: 17.7 MB (59.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int: 
-        mini, maxi, sol = sys.maxsize, 0, 0
+        sol = 0
         for i in range(len(prices)-1):
             if prices[i+1] > prices[i]:
                 sol += prices[i+1] - prices[i]
