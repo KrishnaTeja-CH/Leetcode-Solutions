9c8e1763b63ed8022d5db95b134ca8f21c1ca25c
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-09-14 10:58:23 -0500

    Time: 42 ms (65.78%), Space: 16.2 MB (92.10%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        visit = set()
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSqaures(n)
+            
+            if n == 1:
+                return True
+        return False
+    
+    def sumOfSqaures(self, n: int) -> int:
+        sol = 0
+        while n:
+            digit = n % 10
+            sol+= digit ** 2 
+            n = n // 10
+        return sol
+        
+        
+
+     
