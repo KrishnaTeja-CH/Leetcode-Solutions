5ac9dddb048f864ebaa29175c48a9faddab0c3a3
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-09-14 17:39:14 -0500

    Time: 45 ms (46.89%), Space: 16.1 MB (99.48%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -3,16 +3,16 @@ class Solution:
         visit = set()
         while n not in visit:
             visit.add(n)
-            n = self.sumOfSqaures(n)
-            if n == 1:
-                return True
+            n = self.sumOfSquares(n)
+        
+            if n==1: return True
         return False
     
-    def sumOfSqaures(self, n: int) -> int:
+    def sumOfSquares(self, n: int) -> int:
         sol = 0
         while n:
             digit = n % 10
-            sol+= digit ** 2 
+            sol += digit ** 2
             n = n // 10
         return sol
         
