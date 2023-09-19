17423c80ce2a744a790fb6d99a6021f4b2ffc174
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-09-19 12:06:37 -0500

    Time: 39 ms (78.51%), Space: 16.2 MB (65.25%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -4,18 +4,14 @@ class Solution:
         while n not in visit:
             visit.add(n)
             n = self.sumOfSquares(n)
-        
             if n==1: return True
         return False
     
     def sumOfSquares(self, n: int) -> int:
         sol = 0
-        while n:
+        while n: 
             digit = n % 10
             sol += digit ** 2
             n = n // 10
         return sol
-        
-        
-
      
