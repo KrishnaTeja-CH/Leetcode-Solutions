90f683e84224f39e39159f6c258f9bef1db74b96
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-10-23 17:40:36 -0500

    Time: 48 ms (74.51%), Space: 16.2 MB (86.59%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        k = dict(I= 1, V= 5, X= 10, L= 50, C= 100, D= 500, M =1000)
+        k = dict(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000 )
         s = s.replace("IV", "IIII")
         s = s.replace("IX", "VIIII")
         s = s.replace("XL", "XXXX")
@@ -9,8 +9,10 @@ class Solution:
         s = s.replace("CM", "DCCCC")
         sol = 0
         for i in s:
-             sol += k[i]
+            sol += k[i]
         return sol
+
+    
             
               
         
