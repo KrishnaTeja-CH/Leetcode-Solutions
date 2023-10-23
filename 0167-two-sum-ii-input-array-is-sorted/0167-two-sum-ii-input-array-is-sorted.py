3cc538f3ace4ec55904a0d7ca01ef2696767afd0
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-10-23 09:29:22 -0500

    Time: 115 ms (78.90%), Space: 17.1 MB (95.24%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-# Hashmap
-        sol ={}
-        for i in range(len(numbers)):
-            k = target - numbers[i]
-            if k in sol:
-                return [sol[k]+1, i+1]
-            sol[numbers[i]] = i
-
-# Two Pointer
-'''
-        s, t  = 0, len(numbers)-1
-        while s < t:
-            sol = numbers[s] + numbers[t]
-            if sol == target: return [s+1, t+1]
-            elif sol < target: s += 1
-            else: t -= 1
-                '''                
+        l, r = 0, len(numbers)-1
+        while l < r:
+            sol = numbers[l] + numbers[r]
+            if sol == target:
+                return [l+1, r+1]
+            elif sol > target:
+                r -= 1
+            else:
+                l += 1
+            
