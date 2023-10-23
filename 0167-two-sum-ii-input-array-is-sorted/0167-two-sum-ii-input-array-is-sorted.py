f01af9ff8b748c4c0c06afb9093ff9d20646b8f9
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-10-23 09:31:10 -0500

    Time: 119 ms (58.37%), Space: 17.2 MB (72.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -10,4 +10,13 @@ class Solution:
                 r -= 1
             else:
                 l += 1
+#Hashmap
+        '''
+        sol = {}
+        for i in range(len(numbers)):
+            k = target - numbers[i]
+            if k in sol:
+                return [sol[k]+1, i+1]
+            sol[numbers[i]] = i
+        '''            
          
