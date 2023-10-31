b74622b2aac646c1a5f47a14809f690963b213ae
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-10-31 13:12:19 -0500

    Time: 44 ms (16.05%), Space: 16.4 MB (17.31%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.py
+++ 0163-missing-ranges/0163-missing-ranges.py
@@ -3,9 +3,11 @@ class Solution:
         sol = []
         nums = [lower-1] + nums + [upper+1]
         for i in range(1, len(nums)):
-            if nums[i] > nums[i-1]+1:
+            if nums[i] > nums[i-1] +1:
                 sol.append([nums[i-1]+1, nums[i]-1])
         return sol
+            
+            
     
            
     
