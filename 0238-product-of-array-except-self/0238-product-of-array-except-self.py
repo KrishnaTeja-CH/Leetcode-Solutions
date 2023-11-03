d4415623628e884b2171801ae4314e19dc746edb
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-11-02 23:36:59 -0500

    Time: 191 ms (70.14%), Space: 24 MB (63.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,13 +3,10 @@ class Solution:
         n = len(nums)
         output = [1] * n
         l, r = 1, 1
-
         for i in range(n):
             output[i] *= l
             l *= nums[i]
-
-        for i in range(n - 1, -1, -1):
+        for i in range(n-1,-1, -1):
             output[i] *= r
             r *= nums[i]
-
         return output
