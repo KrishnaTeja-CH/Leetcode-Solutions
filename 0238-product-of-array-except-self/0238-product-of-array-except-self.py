796ebaa62342f9ce30647fc540059539f3f59be5
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-11-02 23:20:56 -0500

    Time: 198 ms (43.79%), Space: 25.6 MB (11.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,13 +3,15 @@ class Solution:
         n = len(nums)
         L, R, Sol = [0] * n, [0] * n, [0] * n
         L[0], R[-1] = 1, 1
-        for i in range(1,n):
-            L[i] = nums[i-1] * L[i-1]
-        for i in reversed(range(n-1)):
-            R[i] = nums[i+1] * R[i+1]
-        for i in range(n):
+        for i in range(1, len(nums)):
+            L[i] = L[i-1] * nums[i-1]
+        for i in reversed(range(len(nums)-1)):
+            R[i] = R[i+1] * nums[i+1]
+        for i in range(len(nums)):
             Sol[i] = L[i] * R[i]
         return Sol
+        
+     
             
             
         
