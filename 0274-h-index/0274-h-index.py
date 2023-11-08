5ec294de21988562c144da84c09af17a165e32c0
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-11-07 20:35:51 -0600

    Time: 38 ms (91.53%), Space: 16.6 MB (23.85%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,7 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         res = 0
-        citations = sorted(citations, reverse = True)
+        citations.sort(reverse = True)
         for i, j in enumerate(citations):
             res = max(res, min(j, i+1))
         return res
