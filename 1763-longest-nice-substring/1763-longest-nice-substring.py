99438c5af2ae694276341c68f86d6670b24ff62e
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-11-22 12:49:14 -0600

    Time: 39 ms (87.31%), Space: 16.5 MB (8.72%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -5,7 +5,7 @@ class Solution:
             if len(string) < 2:
                 return ""
             for i in range(len(string)):
-                if not (string[i].lower() in hashset and string[i].upper() in hashset):
+                if not (string[i].swapcase() in hashset):
                     str1 = dfs(string[:i])
                     str2 = dfs(string[i+1:])
                     return str2 if len(str2) > len(str1) else str1
