1950c8f9f7db3087f5a6f7e4a4d7d10315dc747d
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2023-11-23 22:04:52 -0600

    Time: 1914 ms (28.95%), Space: 20.7 MB (23.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,8 +4,8 @@ class Solution:
         sol = set()
         for i in range(len(nums)):
             first = nums[i]
-            j = i+1
-            k = len(nums)-1 
+            j = i + 1
+            k = len(nums)-1
             while j < k:
                 second = nums[j]
                 third = nums[k]
