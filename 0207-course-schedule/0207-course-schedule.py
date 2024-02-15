30272acce7dfaa16f8e10848622d90c938a46ec2
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-02-14 21:17:50 -0600

    Time: 86 ms (83.86%), Space: 18.3 MB (77.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,38 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
+         
         pre = defaultdict(list)
-        
-        for course, p in prerequisites: 
+        for course, p in prerequisites:
             pre[course].append(p)
+    
+        taken = set()               # We are taking set to see there is no circle                 
         
-        taken = set()
-        
-        def dfs(course):
-            if not pre[course]:
-                return True
-            if course in taken:
-                return False
-            taken.add(course)
+        def dfs(course):            # Create A dfs with the course, and add courses.
             
-            for p in pre[course]:
+            if not pre[course]:     # If pre req of that course is empty we can complete the course.
+                return True
+            
+            if course in taken:     # if course already taken, doesnt work
+                return False
+                
+            taken.add(course)       # Add courses, that are not taken     
+            
+           
+            for p in pre[course]:   # Now check each pre-requsisite of that course
                 if not dfs(p):
                     return False
-            pre[course] = []
+                
+                                    # If all cases are satisfied, upodate the list
+            
+            pre[course] = []        # Now we can complete this course, and this is dfs
             return True
- 
-
+                                    # Now we are calling the dfs from outside
+            
         for course in range(numCourses):
             if not dfs(course):
-                return False
-        return True
+                return False        # We are calling dfs to confirm, there is no circle
         
+        return True                 # If everything qualifies, return True
         
         
