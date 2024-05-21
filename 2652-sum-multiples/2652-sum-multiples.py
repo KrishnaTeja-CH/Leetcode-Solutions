35bb112aa2ddd0c2729714b92b263294a15c6d58
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-05-20 23:55:56 -0500

    Time: 53 ms (91.39%), Space: 16.5 MB (67.14%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -1,9 +1,9 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        res = []
-        for i in range(n+1):
+        sol = 0
+        for i in range(3, n+1):
             if i%3 == 0 or i%5==0 or i%7 == 0:
-                res.append(i)
-        return sum(res)
+                sol += i
+        return sol
        
         
