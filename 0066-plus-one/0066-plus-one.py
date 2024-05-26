62250db08962f90a2a6737caed616c631f31f8d8
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-05-25 23:52:37 -0500

    Time: 35 ms (69.05%), Space: 16.4 MB (84.41%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,5 +2,4 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         sol = int(''.join(str(i) for i in digits))+1
         return [int(i) for i in str(sol)]
-    
-        
+   
