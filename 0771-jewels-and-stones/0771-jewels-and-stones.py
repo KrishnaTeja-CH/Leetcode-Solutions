94d7e949a0edd122a95f132558c9d0a31176f0c8
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-05-28 16:23:42 -0500

    Time: 35 ms (58.73%), Space: 16.7 MB (8.92%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,5 +1,7 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels = set(jewels)
+        print(stones)
         sol = 0
         for i in stones:
             if i in jewels:
