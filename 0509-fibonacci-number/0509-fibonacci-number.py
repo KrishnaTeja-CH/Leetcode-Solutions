bd1d90fd962caa43d93ab92b40ec28e54ddac884
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-06-04 21:24:03 -0500

    Time: 340 ms (24.22%), Space: 16.3 MB (99.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n<0: return n
-        if n==0 : return 0
-        if n==1: return 1   
-        #recursion return self.fib(n-1)+ self.fib(n-2)
-        output, prev1, prev2 = 0,1,0
-        for i in range(2, n+1):
-            output = prev1 + prev2
-            prev2 = prev1
-            prev1 = output
-        return output
+        if n < 2:
+            return n
+        if n >= 2:
+            return self.fib(n-1) + self.fib(n-2)
+            
+        
+            
+        
