e7015f7b62722441db3053147e34939f6d1b6c3b
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-06-04 21:39:34 -0500

    Time: 29 ms (91.90%), Space: 16.5 MB (75.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,12 +3,10 @@ class Solution:
         #Recursion      return n if n<2 else self.fib(n-1) + self.fib(n-2)
         #Memoization
         if n < 2: return n 
-        prev1, prev2, output = 1, 0, 0
-        for i in range(2, n+1):
-            output = prev1 + prev2
-            prev2 = prev1
-            prev1 = output
-        return output
+        prev1, prev2 = 1, 0
+        for _ in range(2, n+1):
+            prev1, prev2 = prev1 + prev2, prev1 
+        return prev1
         
         
             
