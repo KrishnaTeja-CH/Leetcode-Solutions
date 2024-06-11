f8311139915d04b3f81f79d6bbf5b8897f18c7ed
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-06-11 16:36:11 -0500

    Time: 124 ms (51.48%), Space: 21.2 MB (21.35%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals)<2 : return intervals
+        intervals.sort()
+        
+        output = [intervals[0]]
+        for start, end in intervals:
+            if start > output[-1][1]:
+                output.append([start, end])
+            elif end> output[-1][1]:
+                output[-1][1] = end
+        return output
+                        
