2bf0507fbd6b2bb36b31adb8119f3c2b8cb369df
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-06-13 11:08:06 -0500

    Time: 789 ms (57.96%), Space: 17 MB (43.86%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,7 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
+        dp = [0] + [amount+1] * amount
+        print(dp)
         for a in range(1, amount+1):
             for c in coins:
                 if a-c >= 0:
