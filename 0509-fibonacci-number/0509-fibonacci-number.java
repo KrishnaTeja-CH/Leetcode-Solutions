7217f00c34c44c354543491258d07962363ada13
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-08-17 10:17:24 -0500

    Time: 0 ms (100%), Space: 40.6 MB (16.88%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public int fib(int n) {
-        if (n<=1){return n;}
         int prev1 = 1;
         int prev2 = 0;
-        int output = 0;
-        for (int i = 2 ;i<=n;i++){
-            output = prev1 + prev2;
+        int current = 0;
+        if(n <2) return n;
+        for(int i=2;i< n+1; i++ ){
+            current = prev1 + prev2;
             prev2 = prev1;
-            prev1 = output;
-        }
-    return output;
+            prev1 = current;
+        }        
+        return prev1;
     }
 }
