47f111f21a389ac1df89e2ff987bd01b0fa5755f
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-08-20 09:22:16 -0500

    Time: 374 ms (30.56%), Space: 17.8 MB (67.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,19 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # Forward
-        pos = 0
-        for i, n in enumerate(nums):
-            if i > pos:
+        gas  = 0
+        for i, j in enumerate(nums):
+            if i > gas:
                 return False
-            pos = max(pos, i+n)
+            gas = max(gas, i+j)
         return True
 
-#Greedy - Backward
-
-#       pos = len(nums)-1
-#        for i in range(pos, -1, -1):
-#            if i + nums[i] >= pos:
-#                pos = i
-#        return pos == 0 
-
-            
+                
+        
