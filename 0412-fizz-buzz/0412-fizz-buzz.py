8c3d4ffe982f9700e8c8dd508a8934df816722e5
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-08-21 20:08:56 -0500

    Time: 44 ms (48.24%), Space: 17.5 MB (98.51%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -2,13 +2,11 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         sol = []
         for i in range(1, n+1):
-            if i%15 == 0:
-                i = "FizzBuzz"
-            elif i%3 == 0: 
-                i = "Fizz"
-            elif i%5 ==0:
-                i = "Buzz"
-            sol.append(str(i))
+            sol.append(
+            "FizzBuzz"  if i%15 == 0 else    
+            "Fizz" if i%3 == 0 else
+            "Buzz" if i%5 ==0 else
+            str(i))
         return sol
             
         
