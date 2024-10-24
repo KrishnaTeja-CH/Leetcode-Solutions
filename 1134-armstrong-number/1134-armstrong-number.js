3039894e60ca0039bca6600a8fb4811b29168382
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-10-23 23:41:15 -0500

    Time: 0 ms (100.00%), Space: 48.9 MB (50.00%) - LeetHub

---
+++ 1134-armstrong-number/1134-armstrong-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isArmstrong = function(n) {
+    let sol = Array.from(String(n), Number);
+    let res = 0
+    sol.forEach(i=> {
+        res += Math.pow(i, sol.length); 
+    });
+    return res == n;
+}
