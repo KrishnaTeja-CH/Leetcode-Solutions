812173f8464db97273da223a00b2ab49f6296f7f
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2024-10-23 23:30:28 -0500

    Time: 0 ms (100.00%), Space: 16.5 MB (75.85%) - LeetHub

--- 1134-armstrong-number/1134-armstrong-number.py
+++ 1134-armstrong-number/1134-armstrong-number.py
@@ -1,7 +1,9 @@
 class Solution:
     def isArmstrong(self, n: int) -> bool:
-        res = [int(i) for i in str(n)]
-        sol = 0
-        for i in res:
-            sol += i**len(res)
-        return sol == n
+        sol = [int(i) for i in str(n)]
+        res = 0
+        for i in sol:
+            res += (i)**len(sol)
+        return res==n
+            
+        
