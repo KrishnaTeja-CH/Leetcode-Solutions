6e911146b6e178e69c925bb7988601ce8cadbf5e
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2025-04-22 23:09:17 -0500

    Time: 145 ms (5.59%), Space: 18.9 MB (99.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -6,9 +6,11 @@ class Solution:
 
         for i in range(1, n):
             left[i] = max(left[i-1], height[i-1])
+        print("left = " ,left)
 
         for i in range(n-2, -1, -1):
             right[i] = max(right[i+1], height[i+1])
+        print("right =" , right)
 
         for i in range(n):
             res += max(0, min(right[i], left[i]) - height[i])
