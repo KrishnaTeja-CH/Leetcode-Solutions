6df9a5460ae4023802fbaac5a1bc684e9879803d
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2025-08-19 09:44:08 -0500

    Time: 11 ms (89.97%), Space: 18.1 MB (14.1%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,8 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ran, mag = Counter(ransomNote), Counter(magazine)
+        '''ran, mag = Counter(ransomNote), Counter(magazine)
         for i in ran:
             mag[i] = mag.get(i,0)- ran[i]
             if mag[i]< 0 :
                 return False
-        return True
+        return True'''
+        return not (Counter(ransomNote)-Counter(magazine))
