89142a62a559ffc574d39dfc088a29045c9619cd
Krishna Teja Chintala Cheruvu <84757778+KrishnaTeja-CH@users.noreply.github.com>
2025-09-16 09:05:15 -0500

    Time: 0 ms (100%), Space: 17.8 MB (61.36%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -4,7 +4,6 @@ class Solution:
         n2 = sum(nums)
         for i in range(len(nums)):
             n2 -= nums[i]
-            if n1 == n2: 
-                return i
+            if n1 == n2: return i
             n1 += nums[i]
         return -1
